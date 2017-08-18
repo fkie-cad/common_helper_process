60092602d3a803aa61ef68745d718a7fdc46f604
Peter Weidenbach <weidenba@cs.uni-bonn.de>
2017-08-18 14:08:22 +0200

    return code variant added

--- common_helper_process/fail_safe_subprocess.py
+++ common_helper_process/fail_safe_subprocess.py
@@ -10,7 +10,21 @@ def execute_shell_command(shell_command):
     :type shell_command: str
     :return: str
     """
+    return execute_shell_command_get_return_code(shell_command)[0]
+
+
+def execute_shell_command_get_return_code(shell_command):
+    """
+    Execute a shell command and return STDOUT and STDERR in one combined result string.
+    This function shall not raise any errors
+
+    :param shell_command: command to execute
+    :type shell_command: str
+    :return: str, int
+    """
     output = ""
+    rc = 1
     with Popen(shell_command, shell=True, stdout=PIPE, stderr=STDOUT) as pl:
         output = pl.communicate()[0].decode('utf-8', errors='replace')
-    return output
+        rc = pl.returncode
+    return output, rc
--- test/test_fail_safe_subprocess.py
+++ test/test_fail_safe_subprocess.py
@@ -1,9 +1,10 @@
 import unittest
 
 from common_helper_process import execute_shell_command
+from common_helper_process.fail_safe_subprocess import execute_shell_command_get_return_code
 
 
-class Test(unittest.TestCase):
+class TestProcessHelper(unittest.TestCase):
 
     def test_execute_shell_command(self):
         result = execute_shell_command("echo 'test 123'")
@@ -12,3 +13,13 @@ class Test(unittest.TestCase):
     def test_execute_shell_command_error(self):
         result = execute_shell_command("echo 'test 123' 1>&2 && exit 2")
         self.assertEqual(result, 'test 123\n', 'result not correct')
+
+    def test_execute_shell_command_incl_rc(self):
+        output, rc = execute_shell_command_get_return_code("echo 'test 123'")
+        self.assertEqual(output, 'test 123\n', 'result not correct')
+        self.assertEqual(rc, 0, 'return code not correct')
+
+    def test_execute_shell_command_error_incl_rc(self):
+        output, rc = execute_shell_command_get_return_code("echo 'test 123' 1>&2 && exit 2")
+        self.assertEqual(output, 'test 123\n', 'result not correct')
+        self.assertEqual(rc, 2, 'return code not correct')
